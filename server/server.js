ac33c67dc4cb6f40d3c26c0829b73744f4ae12f0
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-08 11:31:29 -0600

    added mutations and authentication with JWT

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const { ApolloServer } = require("apollo-server-express");
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas/index");
 
+const { authMiddleware } = require("./utils/auth");
+
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
@@ -15,7 +17,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    // context: authMiddleware,
+    context: authMiddleware
   });
 
   // Start the Apollo server
