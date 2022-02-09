3ef6f0aa9fbd0c48d03528d1ad7fed98ab17f0be
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-02-09 08:05:28 -0600

    comment to use graphql playground

--- server/server.js
+++ server/server.js
@@ -44,6 +44,8 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+
+// comment this out to use the graphql playground
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 })
